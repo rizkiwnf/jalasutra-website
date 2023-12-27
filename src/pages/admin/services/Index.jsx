f311c7228a338670fa4bad1a1f586524a812c143
iostream-code <aryakmifjannah2603@gmail.com>
2023-12-27 10:30:28 +0700

    creating service index api

--- src/pages/admin/services/Index.jsx
+++ src/pages/admin/services/Index.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import Api from "../../../api/index.jsx"
 import AdminHeader from "../../../components/AdminHeader";
 import { HiSquaresPlus, HiTrash, HiDocumentMagnifyingGlass } from "react-icons/hi2";
-import Pic from "../../../assets/images/service-logo-1.png";
 
 export default function IndexServices() {
+    const [services, setServices] = useState([]);
+
+    const fetchDataServices = async () => {
+        await Api.get('/api/service')
+            .then(response => {
+                setServices(response.data.data.data);
+            })
+    }
+
+    useEffect(() => {
+        fetchDataServices();
+    }, []);
+
     const Title = "Daftar Layanan"
 
     return (
@@ -41,25 +55,37 @@ export default function IndexServices() {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr className="bg-white border-b text-black" key="">
-                                <td className="px-6 py-4">1</td>
-                                <td className="px-6 py-4">BPJS Ketenagakerjaan</td>
-                                <td className="px-6 py-4">Layanan Umum</td>
-                                <td className="px-6 py-4">BPJS Ketenagakerjaan</td>
-                                <td className="px-6 py-4">
-                                    <img src={Pic} alt="" />
-                                </td>
-                                <td className="flex flex-wrap px-6 py-4 items-center gap-2 text-xl">
-                                    <Link to="/admin/services/detail" className="px-2.5 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-blue-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                        <HiDocumentMagnifyingGlass className="w-5 h-5 text-white me-2" />
-                                        Lihat
-                                    </Link>
-                                    <button type="button" className="px-2.5 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
-                                        <HiTrash className="w-5 h-5 text-white me-2" />
-                                        Hapus
-                                    </button>
-                                </td>
-                            </tr>
+                            {
+                                services.length > 0 ?
+                                    services.map((service, index) => (
+                                        <tr className="bg-white border-b text-black" key={index}>
+                                            <td className="px-6 py-4">{index + 1}</td>
+                                            <td className="px-6 py-4">{service.nama}</td>
+                                            <td className="px-6 py-4">{service.jenis}</td>
+                                            <td className="px-6 py-4">{service.deskripsi}</td>
+                                            <td className="px-6 py-4">
+                                                <img src={service.gambar} alt={service.nama} width="200" className="rounded" />
+                                            </td>
+                                            <td className="flex flex-wrap px-6 py-4 items-center gap-2 text-xl">
+                                                <Link to="/admin/services/detail" className="px-2.5 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-blue-600 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                                    <HiDocumentMagnifyingGlass className="w-5 h-5 text-white me-2" />
+                                                    Lihat
+                                                </Link>
+                                                <button type="button" className="px-2.5 py-2 text-xs font-medium text-center inline-flex items-center text-white bg-red-600 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                                                    <HiTrash className="w-5 h-5 text-white me-2" />
+                                                    Hapus
+                                                </button>
+                                            </td>
+                                        </tr>
+                                    )) :
+                                    <tr className="bg-white border-b text-black">
+                                        <td colSpan="6" className="text-center p-4">
+                                            <div className="px-6 py-4 bg-red-400 text-red-700 mb-0 border border-red-700 rounded">
+                                                Data Belum Tersedia!
+                                            </div>
+                                        </td>
+                                    </tr>
+                            }
                         </tbody>
                     </table>
                 </div>
