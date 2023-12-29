f482c44743f5a76c1e6380b6faf344606cce7662
iostream-code <aryakmifjannah2603@gmail.com>
2023-12-29 18:59:59 +0700

    adding some panel in admin index

--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -2,6 +2,8 @@ import { HiUsers, HiEnvelopeOpen, HiFaceSmile } from "react-icons/hi2"
 import { DATA_BERANDA } from "../../data/AdminData"
 import { getStatus } from "../../utils/getStatus"
 import AdminHeader from "../../components/AdminHeader"
+import Profile from "../../assets/images/blank-profile-picture.jpg"
+import { Link } from "react-router-dom"
 
 export default function Home() {
     const Title = "Dashboard"
@@ -31,42 +33,86 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div className="mt-5 bg-white rounded-lg text-black w-full overflow-y-hidden">
-                <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                    <table className="w-full text-sm text-left text-white">
-                        <thead className="text-sm text-white uppercase bg-blue-600">
-                            <tr>
-                                <th scope="col" className="px-6 py-3">
-                                    No
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Tanggal
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Nama
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Keperluan
-                                </th>
-                                <th scope="col" className="px-6 py-3">
-                                    Status
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                DATA_BERANDA.map((data, index) => (
-                                    <tr className="bg-white border-b text-black" key={index}>
-                                        <td className="px-6 py-4">{index + 1}</td>
-                                        <td className="px-6 py-4">{data.date}</td>
-                                        <td className="px-6 py-4">{data.name}</td>
-                                        <td className="px-6 py-4">{data.necessary}</td>
-                                        <td className="px-6 py-4">{getStatus(data.status)}</td>
-                                    </tr>
-                                ))
-                            }
-                        </tbody>
-                    </table>
+            <div className="flex flex-row gap-3 mt-5">
+                <div className="flex-2 bg-white rounded-lg text-black w-full overflow-y-hidden">
+                    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table className="w-full text-sm text-left text-white">
+                            <thead className="text-sm text-white uppercase bg-blue-600">
+                                <tr>
+                                    <th scope="col" className="px-6 py-3">
+                                        Tanggal
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Keperluan
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Status
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    DATA_BERANDA.map((data, index) => (
+                                        <tr className="bg-white border-b text-black" key={index}>
+                                            <td className="px-6 py-4">{data.date}</td>
+                                            <td className="px-6 py-4">{data.necessary}</td>
+                                            <td className="px-6 py-4">{getStatus(data.status)}</td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="flex-1 w-full">
+                    <div className="w-full max-w-md p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div className="flex items-center justify-between mb-4">
+                            <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">Pengguna Terbaru</h5>
+                            <Link to="/admin/users" className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">
+                                Lihat Semua
+                            </Link>
+                        </div>
+                        <div className="flow-root">
+                            <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+                                <li className="py-3 sm:py-4">
+                                    <div className="flex items-center">
+                                        <div className="flex-shrink-0">
+                                            <img className="w-8 h-8 rounded-full" src={Profile} alt="Profile" />
+                                        </div>
+                                        <div className="flex-1 min-w-0 ms-4">
+                                            <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                                Kecamatan
+                                            </p>
+                                            <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                                kecamatan@gmail.com
+                                            </p>
+                                        </div>
+                                        <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                            Admin
+                                        </div>
+                                    </div>
+                                </li>
+                                <li className="py-3 sm:py-4">
+                                    <div className="flex items-center">
+                                        <div className="flex-shrink-0">
+                                            <img className="w-8 h-8 rounded-full" src={Profile} alt="Profile" />
+                                        </div>
+                                        <div className="flex-1 min-w-0 ms-4">
+                                            <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                                                Kecamatan
+                                            </p>
+                                            <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                                                kecamatan@gmail.com
+                                            </p>
+                                        </div>
+                                        <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                            Admin
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
